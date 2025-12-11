13db50eee403409de67cdac95cee9da372c8b965
Xiyan Lin <xlin@byupathway.edu>
2025-12-12 01:28:48 +0800

    week07 Track

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,18 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        // Create 3 activities
+        activities.Add(new Running("03 Nov 2022", 30, 4.8));  // 4.8 km
+        activities.Add(new Cycling("03 Nov 2022", 40, 15.0)); // 15 kph
+        activities.Add(new Swimming("03 Nov 2022", 25, 20)); // 20 laps
+
+        // Display all summaries
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
